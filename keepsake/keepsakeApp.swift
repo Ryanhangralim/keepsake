976fb8e28fee4aa680a4a73957e916722e8f4099
Ryan Hangralim <ryanhangralim@gmail.com>
2025-05-16 09:16:03 +0800

    Initial commit

---
+++ keepsake/keepsakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  keepsakeApp.swift
+//  keepsake
+//
+//  Created by Ryan Hangralim on 16/05/25.
+//
+
+import SwiftUI
+
+@main
+struct keepsakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
